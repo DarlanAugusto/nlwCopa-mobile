bf416d0cd025890050395ab8e5f3a56d4ca9a910
Darlan Silva <darlansilv4@gmail.com>
2022-11-04 20:56:30 -0300

    Criando componente de SignIn e garantindo carregamento de Fontes

--- App.tsx
+++ App.tsx
@@ -1,15 +1,17 @@
-import { StatusBar } from 'expo-status-bar';
-import { NativeBaseProvider, Text, Center } from 'native-base';
+import { NativeBaseProvider } from 'native-base';
+import { useFonts, Roboto_400Regular, Roboto_500Medium, Roboto_700Bold } from '@expo-google-fonts/roboto';
+
+import { SignIn } from './src/screens/SignIn';
+import { Loading } from './src/components/Loading';
 
 import { THEME } from './src/styles/theme';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_500Medium, Roboto_700Bold });
+
   return (
     <NativeBaseProvider theme={THEME}>
-      <Center bgColor="gray.900" flex={1}>
-        <Text color="white" fontSize={24} fontWeight="bold">Hello World!</Text>
-        <StatusBar style="auto" />
-      </Center>
+      {fontsLoaded ? <SignIn /> : <Loading />}
     </NativeBaseProvider>
   );
 }
