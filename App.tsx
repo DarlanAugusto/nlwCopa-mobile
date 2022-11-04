d71d1998985091bf850af398965cd6b76c4e205f
Darlan Silva <darlansilv4@gmail.com>
2022-11-03 21:27:30 -0300

    Importando assets do app

--- App.tsx
+++ App.tsx
@@ -4,7 +4,8 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello World!</Text>
+      <Text style={styles.title}>Hello World!</Text>
+      <Text style={styles.paragraph}>(Aprendendo React Native)</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +14,18 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#000',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  title: {
+    color: '#fff',
+    fontWeight: 'bold',
+    fontSize: 32
+  },
+  paragraph: {
+    color: '#f1f',
+    fontWeight: '500',
+    fontSize: 24
+  }
 });
